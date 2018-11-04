650d9b0c4e5dcda0433adcd2377f4abc913af269
click-here <ragespectate@gmail.com>
2018-11-03 22:23:50 -0400

    bouncing ball start

--- canvas/Project 3/canvas.js
+++ canvas/Project 3/canvas.js
@@ -5,3 +5,26 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+function Circle(x, y, dx, dy, radius, color) {
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.radius = radius;
+
+	this.draw = function() {
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		c.strokeStyle = color
+		c.stroke();
+	}
+}
+var radius = Math.floor(Math.random()*(40-10+1)+10);;
+var x = Math.random() * (innerWidth - radius * 2) + radius;
+var y = (innerHeight - radius * 2) + radius;
+var dx = (Math.random() - 0.5) * 3;
+var dy = (Math.random() - 0.5) * 3;
+
+cir = new Circle(x,y,dx,dy,radius,"red");
+
+cir.draw();
